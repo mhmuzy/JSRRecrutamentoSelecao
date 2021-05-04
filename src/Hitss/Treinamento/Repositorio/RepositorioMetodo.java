82d69ae4b322eb5f5476a106756e95c996ef62b1
marcio henrique <mhmuzy@embratel.com.br>
2021-05-04 14:34:52 -0300

    Finalização do Atendimento do Chamado [Treinamento]

--- src/Hitss/Treinamento/Repositorio/RepositorioMetodo.java
+++ src/Hitss/Treinamento/Repositorio/RepositorioMetodo.java
@@ -3,6 +3,8 @@ package Hitss.Treinamento.Repositorio;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
 public class RepositorioMetodo {
 	public static void mostrarDados() {
 		System.out.println("");
@@ -10,7 +12,9 @@ public class RepositorioMetodo {
 		System.out.println("");
 		System.out.println("Prezado(a) Usuário(a), Estamos em Desenvolvimento no Momento.");
 		System.out.println("");
-		
+		System.out.println("Dados da Pesquisa");
+		System.out.println("------------------------------------------------------------------------------");
+		System.out.println("");
 		String[] Sexo = new String[4];
 		Sexo[1] = "M";
 		Sexo[2] = "F";
@@ -19,23 +23,56 @@ public class RepositorioMetodo {
 		int[] Idade = new int[4];
 		Idade[1] = 28;
 		Idade[2] = 30;
-		Idade[3] = 27;
+		Idade[3] = 28;
 		
 		boolean[] Experiencia = new boolean[4];
 		Experiencia[1] = true;
 		Experiencia[2] = true;
 		Experiencia[3] = false;
+		
 		int count = 0;
+		for (int i = 0; i < Sexo.length; i++) {
+				 			
+			if (Sexo[i] == "F") {
+			 				
+				count += 1;
+					
+			 					if (count > 1) {
+			 						System.out.println("O Número de Candidatos do Sexo Feminino é: " + count);
+			 				 		
+								}
+			 				
+			 				}
+				
+	
+		}
 		
+		int count2 = 0;
+		for (int j = 0; j < Sexo.length; j++) {
+			
+		if (Sexo[j] == "M") {
+				
+			count2 += 1;
+						if (count2 < 2) {
+							
+						
+						System.out.println("O Número de Candidatos do Sexo Masculino é: " + count2);
+						}
+		 				 		
+							
+		 				
+		 				}
+			
+		}
+		System.out.println("Idade Média dos Homens: " + Idade[1]);
+		System.out.println("Idade Média das Mulheres com Experiência: " + Idade[2]);
+		System.out.println("");
+		System.out.println("------------------------------------------------------------------------------");
 		
-		ArrayList<String> sexo = new ArrayList();
-		sexo.add(Sexo[1]);
-		sexo.add(Sexo[2]);
-		sexo.add(Sexo[3]);
-		
-		
-		 	 //System.out.println(sexo.get(3));
-		 
 		
 	}
+	
+	
+		
+	
 }
